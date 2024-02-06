b0b57df754ed4d055f6e12398ac28383fcb5b1ba
bree256 <141923968+bree256@users.noreply.github.com>
2024-02-06 09:12:42 +0300

    fixed button sizing

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const questionAnswers = document.querySelectorAll(".question-answer");
+const questionAnswers = document.querySelectorAll(".question-answer");
+const container = document.querySelector(".container");
 
-  questionAnswers.forEach((questionAnswer) => {
-    const question = questionAnswer.querySelector(".question");
-    const answer = questionAnswer.querySelector(".answer");
-    const iconPlus = questionAnswer.querySelector(".fa-plus");
-    const iconMinus = questionAnswer.querySelector(".fa-minus");
+questionAnswers.forEach((questionAnswer) => {
+  const question = questionAnswer.querySelector(".question");
+  const answer = questionAnswer.querySelector(".answer");
+  const iconPlus = questionAnswer.querySelector(".fa-plus");
+  const iconMinus = questionAnswer.querySelector(".fa-minus");
 
-    question.addEventListener("click", function () {
-      answer.style.display =
-        answer.style.display === "none" || answer.style.display === ""
-          ? "block"
-          : "none";
-      iconPlus.classList.toggle("hide");
-      iconMinus.classList.toggle("hide");
-    });
-  });
+  question.addEventListener("click", clicked);
+  question.addEventListener("click", answerStyle);
+
+  function clicked() {
+    answer.style.display =
+      answer.style.display === "none" || answer.style.display === ""
+        ? "block"
+        : "none";
+
+    iconPlus.classList.toggle("hide");
+    iconMinus.classList.toggle("hide");
+  }
+
+  function answerStyle() {
+    answer.style.paddingBottom = "1rem";
+  }
 });
